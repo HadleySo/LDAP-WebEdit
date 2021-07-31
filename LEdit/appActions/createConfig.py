776c414fc6cf304540d436075366f247dd302ae8
HadleySo <71105018+HadleySo@users.noreply.github.com>
2021-07-30 21:56:33 -0700

    Adding all functions to config creation page

--- LEdit/appActions/createConfig.py
+++ LEdit/appActions/createConfig.py
@@ -1,4 +1,22 @@
 import configparser
 import os
 
-configPathFull = os.path.normpath((__file__) + "../../data/config.txt")
+configPathFull = os.path.normpath((__file__) + "../../../../data/config.txt")
+
+def main(request):
+        try:
+            cfg = configparser.ConfigParser()
+
+            # modify config
+            cfg['LDAP'] = {'ip_address' : request.form['ipAdd']}
+            cfg['CREDENTIALS'] = {'DN' : request.form['DN'],
+                'PW' : request.form['PW']}  
+
+            with open(configPathFull, 'w') as saveConfig:
+                cfg.write(saveConfig)                           # save config
+            
+            return True
+        except Exception as e:
+            print("[ERROR] failed to create config file")
+            print (str(e))
+            return False
--- LEdit/views.py
+++ LEdit/views.py
@@ -6,6 +6,7 @@ def index(name=None):
     print("INFO: Incoming request at root " + request.method)
 
     if request.method == 'POST':
+        # Request to create config or request to abort config creation
         if request.form.get('MainConfig'):
             print("INFO: POST request to Edit Config")
             return render_template('config.html')
@@ -15,6 +16,16 @@ def index(name=None):
     
     return render_template('index.html')    
 
+@app.route('/editConfig',methods = ['POST', 'GET'])
+def config(name=None):
+    print("INFO: Incoming request at /editConfig " + request.method)
+    if request.method == 'POST':
+        from .appActions import createConfig
+        configResult = createConfig.main(request)
+        if (configResult == False):
+            return render_template('index.html', blueMessage = "ERROR in creating config")
+        if (configResult == True):
+            return render_template('index.html', blueMessage = "Success in creating config")
 
 @app.errorhandler(404)
 def page_not_found(error):
